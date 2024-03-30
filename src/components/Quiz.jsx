a1da647afc38dfa6f64e7f7cc138c7ca7af2a029
totty <kaqiu96@gmail.com>
2024-03-30 14:59:46 +0100

    created the quiz component and header component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,3 +1,30 @@
-export const Quiz=()=>{
+import {useState} from "react";
+import QUESTIONS from "../questions.js";
 
+export const Quiz = () => {
+    const [userAnswers, setUserAnswers] = useState([])
+    const activeQuestionIndex = userAnswers.length
+
+    function handelSelectAnswer(selectedAnswer) {
+        setUserAnswers((prevUserAnswer) => {
+            return [...prevUserAnswer, selectedAnswer]
+        })
+
+    }
+
+    return (
+        <div id='quiz'>
+            <div id='question'>
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id='answers'>
+                    {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                        <li key={answer} className='answer'>
+                            <button onClick={() => handelSelectAnswer(answer)}>{answer}</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+
+    )
 }
